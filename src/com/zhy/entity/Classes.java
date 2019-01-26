ce799b05d64c9724adbd1e284ab1e09893a4fd82
zhy139423 <1605601028@qq.com>
2019-01-26 12:10:35 +0800

    First

---
+++ src/com/zhy/entity/Classes.java
@@ -0,0 +1,47 @@
+package com.zhy.entity;
+
+import java.util.List;
+
+public class Classes {
+	private int id;
+	private String name;
+	private List<Student> students;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Classes() {
+
+	}
+
+	public Classes(int id, String name, List<Student> students) {
+		this.id = id;
+		this.name = name;
+		this.students = students;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	@Override
+	public String toString() {
+		return "Classes{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", students=" + students +
+				'}';
+	}
+}
