5ca011094e354da9eca42c96a64b6cd6e037f0b1
zhy139423 <1605601028@qq.com>
2019-01-26 13:07:43 +0800

    www

---
+++ src/com/zhy/dao/ClassesDao.java
@@ -0,0 +1,12 @@
+package com.zhy.dao;
+
+import com.zhy.entity.Classes;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2019/1/25.
+ */
+public interface ClassesDao {
+    List<Classes> selectAll();
+}
